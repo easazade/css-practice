ca4786c223ceab572673f8fbc0d75815225943c1
alireza <easazade@gmail.com>
2019-09-09 18:11:31 +0430

    adding more examples

--- index.php
+++ index.php
@@ -3,8 +3,22 @@
 <head>
   <meta charset="UTF-8">
   <title>Css Tutorial</title>
+  <style>
+    a, a:hover a:visited a:active{
+      background-color: #933098;
+      color: white;
+      border-radius: 3px;
+      border: 3px solid #633f80;
+      padding: 10px;
+      text-decoration: none;
+      cursor: pointer;
+    }
+  </style>
 </head>
 <body>
-hello css
+<br>
+<a href="padding.html">padding</a>
+<a href="height_and_width.html">height and width</a>
+<a href="box_model.html">box model</a>
 </body>
 </html>
