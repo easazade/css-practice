d6a07221ba47d7e7b87315f4d4b135ca0feaf292
alireza <easazade@gmail.com>
2019-09-10 19:05:40 +0430

    adding more examples lists font icons text ...

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <title>Css Tutorial</title>
   <style>
-    a, a:hover a:visited a:active{
+    a, a:hover a:visited a:active {
       background-color: #933098;
       color: white;
       border-radius: 3px;
@@ -17,8 +17,22 @@
 </head>
 <body>
 <br>
+<p>
+  only some of the examples are written in html pages and are browsable the rest are just simple css files.
+  check them out in the source code
+</p>
+<br>
+<br>
+<br>
 <a href="padding.html">padding</a>
 <a href="height_and_width.html">height and width</a>
 <a href="box_model.html">box model</a>
+<a href="image_text_alignment.html">image text alignment</a>
+<a href="font_icon.html">font icon</a>
+<a href="bootstrap_icons.html">Bootstrap Icons</a>
+<a href="google_icons.html">Google Icons</a>
+<a href="cursors.html">Cursors</a>
+<a href="lists.html">Lists</a>
+<a href="styled_lists.html">Styled Lists</a>
 </body>
 </html>
